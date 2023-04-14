7e1491efe8b31051ef63703f33db089ad425ce67
uname-n <git@defend.sh>
2023-04-14 07:56:08 -0700

    update

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,46 @@
-use std::io::BufWriter;
-use std::io::Write;
 use std::fs::File;
+use std::io::{BufWriter, Write};
+use std::path::Path;
 
 fn main() {
-    let mut context = BufWriter::new(File::create("Context.md").unwrap());
-    
-    for file in std::fs::read_dir("src").unwrap() {
-        let file = file.unwrap();
-        let file_name = file.file_name().into_string().unwrap();
+    let output_file = File::create("Context.md").expect("Failed to create Context.md");
+    let mut context = BufWriter::new(output_file);
 
-        let content = std::fs::read_to_string(file.path()).unwrap();
-        let content = format!("## {}\n```rust\n{}\n```\n", file_name, content);
-        
-        context.write_all(content.as_bytes()).unwrap();
+    // Process the "src" directory
+    process_directory(&mut context, "src").expect("Failed to process src directory");
+
+    // Process the "tests" directory, if it exists
+    if Path::new("tests").exists() {
+        process_directory(&mut context, "tests").expect("Failed to process tests directory");
     }
 
-    for file in std::fs::read_dir("tests").unwrap() {
-        let file = file.unwrap();
-        let file_name = file.file_name().into_string().unwrap();
+    drop(context);
 
-        let content = std::fs::read_to_string(file.path()).unwrap();
-        let content = format!("## {}\n```rust\n{}\n```\n", file_name, content);
-        
-        context.write_all(content.as_bytes()).unwrap();
+    // Run "cargo check" and append its output to Context.md
+    std::process::Command::new("sh")
+        .arg("-c")
+        .arg("cargo check 2>&1 | tee -a Context.md")
+        .output()
+        .expect("Failed to run cargo check");
+}
+
+fn process_directory(context: &mut BufWriter<File>, dir: &str) -> std::io::Result<()> {
+    let entries = std::fs::read_dir(dir)?;
+
+    for entry in entries {
+        let entry = entry?;
+        let file_name = entry.file_name().into_string().map_err(|_| {
+            std::io::Error::new(
+                std::io::ErrorKind::InvalidData,
+                format!("Invalid filename in {} directory", dir),
+            )
+        })?;
+
+        let content = std::fs::read_to_string(entry.path())?;
+        let formatted_content = format!("## {}\n```rust\n{}\n```\n", file_name, content);
+
+        context.write_all(formatted_content.as_bytes())?;
     }
+
+    Ok(())
 }
